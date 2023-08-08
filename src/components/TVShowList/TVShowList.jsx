8f247548bc7a3ff2670885a528f40188e5fbbf56
wyllwong95 <wwong95@hotmail.co.uk>
2023-08-08 12:19:16 +0100

    fix text scroll bug

--- src/components/TVShowList/TVShowList.jsx
+++ src/components/TVShowList/TVShowList.jsx
@@ -3,19 +3,21 @@ import s from "./style.module.css";
 
 export function TVShowList({ tvShowList, onClickItem }) {
   return (
-    <div className={s.tvshow_wrapper}>
+    <>
       <div className={s.title}>You'll probably like :</div>
-      <div className={s.list}>
-        {tvShowList.map((tvShow) => {
-          return (
-            <TVShowListItem
-              tvShow={tvShow}
-              onClick={onClickItem}
-              key={tvShow.id}
-            ></TVShowListItem>
-          );
-        })}
+      <div className={s.tvshow_wrapper}>
+        <div className={s.list}>
+          {tvShowList.map((tvShow) => {
+            return (
+              <TVShowListItem
+                tvShow={tvShow}
+                onClick={onClickItem}
+                key={tvShow.id}
+              ></TVShowListItem>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }

fbc94618c3ade103e057726cd365831fca36de24
wyllwong95 <wwong95@hotmail.co.uk>
2023-08-08 12:05:48 +0100

    fix jumping bug

--- src/components/TVShowList/TVShowList.jsx
+++ src/components/TVShowList/TVShowList.jsx
@@ -3,7 +3,7 @@ import s from "./style.module.css";
 
 export function TVShowList({ tvShowList, onClickItem }) {
   return (
-    <div>
+    <div className={s.tvshow_wrapper}>
       <div className={s.title}>You'll probably like :</div>
       <div className={s.list}>
         {tvShowList.map((tvShow) => {
